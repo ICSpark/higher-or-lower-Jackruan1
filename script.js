362410d2be4ff8996862fa1e2a9e8159447b0706
Jackruan1 <jackruan0303@gmail.com>
2025-02-08 21:28:51 +0000

    finished

--- script.js
+++ script.js
@@ -16,12 +16,32 @@ function generateRandom() {
 
 // Function for the higher button
 function higher() {
+  if (random > nextRandom) {
+    window.alert ("womp womp")
+  }
+  else if (random < nextRandom) {
+    window.alert ("you win!")
+  }
+  else {
+    window.alert ("it's the same! (1 in 10,000)!")
+  }
+  generateRandom();
    
 }
 
 // Function for the lower button
 function lower() {
-   
+   if (random > nextRandom) {
+    window.alert ("you win!")
+   }
+   else if (random < nextRandom) {
+    window.alert ("womp womp")
+   }
+   else {
+    window.alert ("it's the same! (1 in 10,000)!")
+   }
+   generateRandom();
+
 }
 
 //Generate a number to start the game!
